2bb2ff3536d72c9a80d767d776e1df62c6c7efb5
Samuel <samuelpeybernes33@gmail.com>
2024-03-09 14:51:21 +0100

    new stepper

---
+++ components/Stepper.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="flex flex-wrap ">
+        <div class="relative mt-12 lg:mt-20 w-full">
+            <div class="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28 mt-12">
+                <svg class="w-full" xmlns="http://www.w3.org/2000/svg" width="875" height="48" viewBox="0 0 875 48"
+                    fill="none">
+                    <path
+                        d="M2 29C20.2154 33.6961 38.9915 35.1324 57.6111 37.5555C80.2065 40.496 102.791 43.3231 125.556 44.5555C163.184 46.5927 201.26 45 238.944 45C312.75 45 385.368 30.7371 458.278 20.6666C495.231 15.5627 532.399 11.6429 569.278 6.11109C589.515 3.07551 609.767 2.09927 630.222 1.99998C655.606 1.87676 681.208 1.11809 706.556 2.44442C739.552 4.17096 772.539 6.75565 805.222 11.5C828 14.8064 850.34 20.2233 873 24"
+                        stroke="#805b37" stroke-width="4" stroke-linecap="round" stroke-dasharray="1 12" />
+                </svg>
+            </div>
+        </div>
+        <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-75 mx-auto mb-8 shadow-lg rounded-lg">
+                <div class="px-4 py-5 flex-auto">
+                    <div
+                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                        <span class="text-xl font-semibold text-gray-700 dark:text-gray-200">1</span>
+                    </div>
+                    <h6 class="text-xl font-semibold">Awarded Agency</h6>
+                    <p class="mt-2 mb-4 text-blueGray-500">
+                        Divide details about your product or agency work into parts.
+                        A paragraph describing a feature will be enough.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-75 mx-auto mb-8 shadow-lg rounded-lg">
+                <div class="px-4 py-5 flex-auto">
+                    <div
+                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                        <span class="text-xl font-semibold text-gray-700 dark:text-gray-200">2</span>
+                    </div>
+                    <h6 class="text-xl font-semibold">Free Revisions</h6>
+                    <p class="mt-2 mb-4 text-blueGray-500">
+                        Keep you user engaged by providing meaningful information.
+                        Remember that by this time, the user is curious.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pt-6 w-full md:w-4/12 px-4 text-center z-2">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-75 mx-auto mb-8 shadow-lg rounded-lg">
+                <div class="px-4 py-5 flex-auto">
+                    <div
+                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                        <span class="text-xl font-semibold text-gray-700 dark:text-gray-200">3</span>
+                    </div>
+                    <h6 class="text-xl font-semibold">Verified Company</h6>
+                    <p class="mt-2 mb-4 text-blueGray-500">
+                        Write a few lines about each one. A paragraph describing a
+                        feature will be enough. Keep you user engaged!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="relative w-full">
+        <div class="absolute inset-x-0 hidden top-2 md:block left-[65%] rotate-[270deg]">
+            <svg class="w-full" xmlns="http://www.w3.org/2000/svg" width="200" height="48" viewBox="0 0 200 48" fill="none">
+                <path
+                    d="M2 29C20.2154 33.6961 38.9915 35.1324 57.6111 37.5555C80.2065 40.496 102.791 43.3231 125.556 44.5555C163.184 46.5927 201.26 45 238.944 45C312.75 45 385.368 30.7371 458.278 20.6666C495.231 15.5627 532.399 11.6429 569.278 6.11109C589.515 3.07551 609.767 2.09927 630.222 1.99998C655.606 1.87676 681.208 1.11809 706.556 2.44442C739.552 4.17096 772.539 6.75565 805.222 11.5C828 14.8064 850.34 20.2233 873 24"
+                    stroke="#805b37" stroke-width="4" stroke-linecap="round" stroke-dasharray="1 12" />
+            </svg>
+        </div>
+    </div>
+    <div class="flex flex-wrap flex-row-reverse	">
+        <div class="relative mt-12 lg:mt-20 w-full">
+            <div class="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28 mt-12 transform skew-y-180">
+                <svg class="w-full" xmlns="http://www.w3.org/2000/svg" width="875" height="48" viewBox="0 0 875 48"
+                    fill="none">
+                    <path
+                        d="M2 29C20.2154 33.6961 38.9915 35.1324 57.6111 37.5555C80.2065 40.496 102.791 43.3231 125.556 44.5555C163.184 46.5927 201.26 45 238.944 45C312.75 45 385.368 30.7371 458.278 20.6666C495.231 15.5627 532.399 11.6429 569.278 6.11109C589.515 3.07551 609.767 2.09927 630.222 1.99998C655.606 1.87676 681.208 1.11809 706.556 2.44442C739.552 4.17096 772.539 6.75565 805.222 11.5C828 14.8064 850.34 20.2233 873 24"
+                        stroke="#805b37" stroke-width="4" stroke-linecap="round" stroke-dasharray="1 12" />
+                </svg>
+            </div>
+        </div>
+        <div class=" w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-75 mx-auto mb-8 shadow-lg rounded-lg">
+                <div class="px-4 py-5 flex-auto">
+                    <div
+                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                        <span class="text-xl font-semibold text-gray-700 dark:text-gray-200">4</span>
+                    </div>
+                    <h6 class="text-xl font-semibold">Awarded Agency</h6>
+                    <p class="mt-2 mb-4 text-blueGray-500">
+                        Divide details about your product or agency work into parts.
+                        A paragraph describing a feature will be enough.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full lg:pt-12 pt-6 md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-75 mx-auto mb-8 shadow-lg rounded-lg">
+                <div class="px-4 py-5 flex-auto">
+                    <div
+                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                        <span class="text-xl font-semibold text-gray-700 dark:text-gray-200">5</span>
+                    </div>
+                    <h6 class="text-xl font-semibold">Free Revisions</h6>
+                    <p class="mt-2 mb-4 text-blueGray-500">
+                        Keep you user engaged by providing meaningful information.
+                        Remember that by this time, the user is curious.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class=" w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-75 mx-auto mb-8 shadow-lg rounded-lg">
+                <div class="px-4 py-5 flex-auto">
+                    <div
+                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                        <span class="text-xl font-semibold text-gray-700 dark:text-gray-200">6</span>
+                    </div>
+                    <h6 class="text-xl font-semibold">Verified Company</h6>
+                    <p class="mt-2 mb-4 text-blueGray-500">
+                        Write a few lines about each one. A paragraph describing a
+                        feature will be enough. Keep you user engaged!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+            mySteps: ['Step 1', 'Step 2', 'Step 3']
+        }
+    },
+
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -30,58 +30,11 @@
           </svg>
         </div>
       </div>
-
       <section class="pb-20 bg-blueGray-200 -mt-24">
         <div class="container mx-auto px-4">
-          <div class="flex flex-wrap">
-            <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-              <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div class="px-4 py-5 flex-auto">
-                  <div
-                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                    <i class="fas fa-award"></i>
-                  </div>
-                  <h6 class="text-xl font-semibold">Awarded Agency</h6>
-                  <p class="mt-2 mb-4 text-blueGray-500">
-                    Divide details about your product or agency work into parts.
-                    A paragraph describing a feature will be enough.
-                  </p>
-                </div>
-              </div>
-            </div>
+          <Stepper />
 
-            <div class="w-full md:w-4/12 px-4 text-center">
-              <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div class="px-4 py-5 flex-auto">
-                  <div
-                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                    <i class="fas fa-retweet"></i>
-                  </div>
-                  <h6 class="text-xl font-semibold">Free Revisions</h6>
-                  <p class="mt-2 mb-4 text-blueGray-500">
-                    Keep you user engaged by providing meaningful information.
-                    Remember that by this time, the user is curious.
-                  </p>
-                </div>
-              </div>
-            </div>
 
-            <div class="pt-6 w-full md:w-4/12 px-4 text-center">
-              <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div class="px-4 py-5 flex-auto">
-                  <div
-                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
-                    <i class="fas fa-fingerprint"></i>
-                  </div>
-                  <h6 class="text-xl font-semibold">Verified Company</h6>
-                  <p class="mt-2 mb-4 text-blueGray-500">
-                    Write a few lines about each one. A paragraph describing a
-                    feature will be enough. Keep you user engaged!
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
 
           <div class="flex flex-wrap items-center mt-32">
             <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
@@ -357,6 +310,7 @@
 <script >
 import Navbar from "@/components/Navbars/IndexNavbar.vue";
 import FooterComponent from "@/components/Footers/FooterSmall.vue";
+import Stepper from "@/components/Stepper.vue";
 import Sponsors from "@/components/Sponsors.vue";
 import team1 from "@/assets/img/team-1-800x800.jpg";
 import team2 from "@/assets/img/team-2-800x800.jpg";
